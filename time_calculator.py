bc9445f2fb39f8e1cf7fc35101003788ddd93847
Marina Martynenko <alivemary@gmail.com>
2020-12-26 21:40:58 +0100

    Refactor with class

--- time_calculator.py
+++ time_calculator.py
@@ -1,10 +1,23 @@
 from datetime import datetime, timedelta
 
 
-def add_time(start, duration, weekday=None):
+class TimeProcessor:
+    def __init__(self, start, duration, weekday=None):
+        self.show_weekday = weekday is not None
+        self.start = self.__get_start_date(start, weekday)
+        self.end = self.__get_end_date(duration)
 
-    start_time = datetime.strptime(start, "%I:%M %p")
-    if weekday is not None:
+    def __str__(self):
+        delta = self.end.date() - self.start.date()
+        appendix = "" if delta.days == 0 else " (next day)"
+        if delta.days > 1:
+            appendix = f" ({delta.days} days later)"
+        return datetime.strftime(
+            self.end, f"%#I:%M %p{'' if not self.show_weekday else ', %A'}{appendix}"
+        )
+
+    def __get_start_date(self, start, weekday):
+        parsed_start = datetime.strptime(start, "%I:%M %p")
         weekdays = (
             "monday",
             "tuesday",
@@ -14,20 +27,17 @@ def add_time(start, duration, weekday=None):
             "saturday",
             "sunday",
         )
-        delta = weekdays.index(weekday.lower()) - start_time.weekday()
-        start_time += timedelta(days=delta)
+        days = (
+            weekdays.index(weekday.lower()) - parsed_start.weekday()
+            if self.show_weekday
+            else 0
+        )
+        return parsed_start + timedelta(days=days)
 
-    [hours, minutes] = duration.split(":")
+    def __get_end_date(self, duration):
+        [hours, minutes] = duration.split(":")
+        return self.start + timedelta(hours=int(hours), minutes=int(minutes))
 
-    end_time = start_time + timedelta(hours=int(hours), minutes=int(minutes))
 
-    days_diff = end_time.date() - start_time.date()
-
-    appendix = "" if days_diff.days == 0 else " (next day)"
-
-    if days_diff.days > 1:
-        appendix = f" ({days_diff.days} days later)"
-
-    return datetime.strftime(
-        end_time, f"%#I:%M %p{'' if weekday is None else ', %A'}{appendix}"
-    )
+def add_time(start, duration, weekday=None):
+    return str(TimeProcessor(start, duration, weekday))
